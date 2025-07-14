d5c63959f6b46211d8cd47393ff808ed1c8c9737
smhasnanmonir <monirhasnan@gmail.com>
2025-07-14 22:37:10 +0600

    text aligned after some fighting

--- frontend/src/app/components/shared/Cards/ProductCard.tsx
+++ frontend/src/app/components/shared/Cards/ProductCard.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-
 import Link from "next/link";
 
 const ProductCard = ({
@@ -15,36 +14,32 @@ const ProductCard = ({
 }) => {
   return (
     <div className="flex flex-col h-full w-full rounded-md p-[8px] md:p-[16px] cursor-pointer border-[2px] border-gray-200/40 inset-shadow-indigo-500/50 hover:border-pink-200 transition-all duration-300">
-      <img
-        className="aspect-square w-full h-[220px] object-cover rounded-lg"
-        src={img}
-        alt=""
-      />
-      <div className="flex flex-col flex-grow space-y-3 text-center mt-4">
-        <h1 className="font-semibold text-sm leading-tight text-left w-3/4">
+      <div className="w-full aspect-square overflow-hidden rounded-lg">
+        <img className="w-full h-full object-cover" src={img} alt="" />
+      </div>
+      <div className="w-full flex flex-col flex-1 justify-between mt-4">
+        <h1 className="font-semibold text-sm leading-tight w-full text-left md:min-h-0 min-h-[50px]">
           {name}
         </h1>
-        <p className="font-medium text-gray-700 text-left">{price} Taka</p>
-        <div className="md:flex gap-1.5 items-center justify-center space-y-2">
-          {/* Button Div to make them side by side */}
+        <p className="font-medium text-gray-700 w-full text-left">{price}</p>
 
+        <div className="grid md:grid-cols-2 gap-[8px] w-full">
           {/* Add to Cart Button */}
-          <Link href="" className="mt-auto flex-1/2 cursor-pointer block">
-            <button className="cursor-pointer block rounded px-5 py-2.5 overflow-hidden group bg-pink-500 hover:bg-gradient-to-r hover:from-pink-500 hover:to-pink-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-pink-400 transition-all ease-out duration-300 text-center w-full mx-auto relative">
-              <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
-              <span className="relative font-semibold">Add to Cart</span>
-            </button>
+          <Link
+            href=""
+            className="block cursor-pointer rounded px-5 py-2.5 overflow-hidden group bg-pink-500 hover:bg-gradient-to-r hover:from-pink-500 hover:to-pink-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-pink-400 transition-all ease-out duration-300 text-center w-full mx-auto relative"
+          >
+            <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+            <span className="relative font-semibold">Add to Cart</span>
           </Link>
 
           {/* View Details Button */}
           <Link
             href={`/products/${slug}`}
-            className="mt-auto flex-1/2 block cursor-pointer"
+            className="cursor-pointer block rounded px-5 py-2.5 overflow-hidden group bg-[#FFF300] hover:bg-gradient-to-r hover:from-[#FFF300] hover:to-[#FFF700] text-black hover:ring-2 hover:ring-offset-2 hover:ring-pink-400 transition-all ease-out duration-300 text-center w-full mx-auto relative"
           >
-            <button className="cursor-pointer block rounded px-5 py-2.5 overflow-hidden group bg-[#FFF300] hover:bg-gradient-to-r hover:from-[#FFF300] hover:to-[#FFF700] text-black hover:ring-2 hover:ring-offset-2 hover:ring-pink-400 transition-all ease-out duration-300 text-center w-full mx-auto relative">
-              <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
-              <span className="relative font-semibold">View Details</span>
-            </button>
+            <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+            <span className="relative font-semibold">View Details</span>
           </Link>
         </div>
       </div>

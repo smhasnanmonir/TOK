da17a9d06dcf84b0c02fe1ed6a20a162304daf31
smhasnanmonir <monirhasnan@gmail.com>
2025-07-17 13:34:17 +0600

    product page mobile friendly

--- frontend/src/app/components/ProductComponents/ProductTabs.tsx
+++ frontend/src/app/components/ProductComponents/ProductTabs.tsx
@@ -8,7 +8,7 @@ const ProductTabs = ({ product }: { product: any }) => {
   const benefits = JSON.parse(product?.result?.details?.benefits);
   //   const reviews = JSON.parse(product?.result?.details?.reviews);
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6">
+    <div className="">
       <Tabs defaultValue="account">
         <TabsList>
           <TabsTrigger value="account">How To Use</TabsTrigger>
@@ -17,7 +17,7 @@ const ProductTabs = ({ product }: { product: any }) => {
           <TabsTrigger value="reviews">Reviews</TabsTrigger>
         </TabsList>
         <TabsContent value="account">
-          <Card className="p-4 w-full min-h[40px] block">
+          <Card className="p-4 h-[220px] block transition-all duration-300">
             <div>
               {howToUse.map((item: any, i: number) => (
                 <div className="flex flex-col " key={i + 10}>
@@ -30,7 +30,7 @@ const ProductTabs = ({ product }: { product: any }) => {
           </Card>
         </TabsContent>
         <TabsContent value="benefits">
-          <Card className="p-4 w-full min-h[40px] block">
+          <Card className="p-4 h-[220px] block transition-all duration-300">
             <div className="">
               {benefits.map((item: any, i: number) => (
                 <div key={i + 20}>
@@ -43,12 +43,14 @@ const ProductTabs = ({ product }: { product: any }) => {
           </Card>
         </TabsContent>
         <TabsContent value="key-ingredients">
-          <Card className="p-4 w-full min-h[40px] block">
+          <Card className="p-4 h-[220px] block transition-all duration-300">
             <div className="">{product?.result?.details?.key_ingredient}</div>
           </Card>
         </TabsContent>
         <TabsContent value="reviews">
-          <Card></Card>
+          <Card className="h-[220px] transition-all duration-300 p-4">
+            <h1>No reviews yet</h1>
+          </Card>
         </TabsContent>
       </Tabs>
     </div>
--- frontend/src/app/products/[productSlug]/page.tsx
+++ frontend/src/app/products/[productSlug]/page.tsx
@@ -41,31 +41,33 @@ const ProductDetails = async ({
   const skinConcernArray: string[] = product?.result?.skin_concern.split(",");
 
   return (
-    <div className="md:pt-[96px] pt-[54px] max-w-7xl mx-auto overflow-hidden mb-[50px]">
-      <div className="mx-auto px-[4%] grid grid-cols-2 gap-4 items-stretch ">
-        <div className="h-full">
+    <div className="md:pt-[96px] pt-[54px] max-w-7xl mx-auto overflow-hidden md:px-0">
+      <div className="mx-auto px-[2%] grid grid-cols-1 md:grid-cols-2 gap-4 items-stretch ">
+        <div className="h-full flex justify-center items-center">
           <img
-            className="w-[550px] h-full object-cover rounded-md"
+            className="w-full h-full object-cover rounded-md"
             src={product?.result?.img}
             alt={product?.result?.name}
           />
         </div>
-        <div className="flex flex-col space-y-[24px]">
-          <h1 className="text-2xl font-semibold">{product?.result?.name}</h1>
-          <p className="text-xl text-[#DE3163] font-semibold">
+        <div className="flex flex-col space-y-[16px] sm:space-y-[20px] md:space-y-[24px] px-2 sm:px-4 md:px-0">
+          <h1 className="text-lg sm:text-xl md:text-2xl font-semibold">
+            {product?.result?.name}
+          </h1>
+          <p className="text-base sm:text-lg md:text-xl text-[#DE3163] font-semibold">
             ৳ {product?.result?.price}{" "}
-            <span className="line-through text-gray-400 text-sm">
+            <span className="line-through text-gray-400 text-xs sm:text-sm">
               {Number(product?.result?.price) + 200}{" "}
             </span>
           </p>
-          <h1 className="text-[17px]">
+          <h1 className="text-[15px] sm:text-[16px] md:text-[17px]">
             <span className="font-semibold">Brand : </span>
             <span className="font-extrabold">{product?.result?.brand}</span>
           </h1>
-          <h1 className="text-[16px] text-gray w-3/4">
+          <h1 className="text-[14px] sm:text-[15px] md:text-[16px] text-gray w-full sm:w-3/4">
             {product?.result?.details?.description}
           </h1>
-          <h1 className={`text-[17px] $`}>
+          <h1 className={`text-[15px] sm:text-[16px] md:text-[17px] $`}>
             <span className="font-semibold">Status: </span>{" "}
             <span
               className={` ${
@@ -78,7 +80,7 @@ const ProductDetails = async ({
           <div className="flex flex-wrap gap-2">
             {skinConcernArray?.map((concern: string) => (
               <button
-                className="font-semibold text-center text-sm px-[12px] py-[4px] border border-gray-200"
+                className="font-semibold text-center text-xs sm:text-sm px-[10px] sm:px-[12px] py-[4px] border border-gray-200"
                 key={concern}
               >
                 {concern}
@@ -86,7 +88,9 @@ const ProductDetails = async ({
             ))}
           </div>
           <h1>
-            <span className="font-semibold text-[17px]">Skin Type : </span>
+            <span className="font-semibold text-[15px] sm:text-[17px]">
+              Skin Type :{" "}
+            </span>
             <span className="">{product?.result?.skin_type}</span>
           </h1>
           <div className="w-full h-[0.5px] bg-gray-200"></div>
@@ -94,22 +98,23 @@ const ProductDetails = async ({
           <div className="flex flex-wrap gap-2">
             {parsedSizeArray?.map((size: number) => (
               <button
-                className=" cursor-pointer font-semibold text-center text-xl px-[18px] py-[4px] border border-gray-200"
+                className=" cursor-pointer font-semibold text-center text-base sm:text-xl px-[14px] sm:px-[18px] py-[4px] border border-gray-200"
                 key={size}
               >
                 {size} ml
               </button>
             ))}
           </div>
-          <div className="flex gap-2 w-1/2">
+          <div className="flex gap-2 w-full sm:w-1/2">
             <AddToCartButton></AddToCartButton>
             <BuyNowButton></BuyNowButton>
           </div>
         </div>
       </div>
-      <div className="my-[24px] w-full grid place-content-center">
+      <div className="my-[24px] w-full grid place-content-center px-2 sm:px-4 md:px-0">
         <ProductTabs product={product}></ProductTabs>
       </div>
+      <div className="w-full h-[1px] bg-gray-200"></div>
     </div>
   );
 };

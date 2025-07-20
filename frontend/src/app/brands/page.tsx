dd6c81934129dd837da3fe2f1d7f38633d7abc54
smhasnanmonir <monirhasnan@gmail.com>
2025-07-20 18:30:16 +0600

    loading state added

--- frontend/src/app/brands/page.tsx
+++ frontend/src/app/brands/page.tsx
@@ -30,34 +30,42 @@ const page = async ({
     : brands.result;
 
   return (
-    <div className="md:pt-[80px] pt-[58px] ">
-      <div className="bg-pink-500/20 px-[20px] py-[24px] flex flex-col space-y-[12px]">
-        <h1 className="md:text-center md:text-2xl font-semibold">
-          TOK offers products from top Korean brands
-        </h1>
-        <p className="md:text-center md:text-xl text-sm text-gray-500">
-          Straightly for Korea! Buy with confidence.
-        </p>
-      </div>
-      <div className="px-[3%]">
-        <div>
-          <div className="my-[12px]">
-            <div className="flex flex-col space-y-[12px]">
-              <SearchBrands />
+    <Suspense
+      fallback={
+        <div className="grid place-content-center h-screen">
+          <div>Loading...</div>
+        </div>
+      }
+    >
+      <div className="md:pt-[80px] pt-[58px] ">
+        <div className="bg-pink-500/20 px-[20px] py-[24px] flex flex-col space-y-[12px]">
+          <h1 className="md:text-center md:text-2xl font-semibold">
+            TOK offers products from top Korean brands
+          </h1>
+          <p className="md:text-center md:text-xl text-sm text-gray-500">
+            Straightly for Korea! Buy with confidence.
+          </p>
+        </div>
+        <div className="px-[3%]">
+          <div>
+            <div className="my-[12px]">
+              <div className="flex flex-col space-y-[12px]">
+                <SearchBrands />
+              </div>
             </div>
           </div>
+          <Suspense
+            fallback={<div className="grid place-items-center">Loading...</div>}
+          >
+            <div className="grid grid-cols-2 lg:grid-cols-6 md:grid-cols-4 gap-[12px]">
+              {filteredBrands.map((brand: brandType) => (
+                <TypeCard key={brand?.slug} props={brand}></TypeCard>
+              ))}
+            </div>
+          </Suspense>
         </div>
-        <Suspense
-          fallback={<div className="grid place-items-center">Loading...</div>}
-        >
-          <div className="grid grid-cols-2 lg:grid-cols-6 md:grid-cols-4 gap-[12px]">
-            {filteredBrands.map((brand: brandType) => (
-              <TypeCard key={brand?.slug} props={brand}></TypeCard>
-            ))}
-          </div>
-        </Suspense>
       </div>
-    </div>
+    </Suspense>
   );
 };
 

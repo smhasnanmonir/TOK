1e822f7884bf804f28379547d1e1c6d27f14d204
smhasnanmonir <monirhasnan@gmail.com>
2025-08-01 01:46:21 +0600

    streaming added and removed loading js

--- frontend/src/app/brands/page.tsx
+++ frontend/src/app/brands/page.tsx
@@ -1,8 +1,5 @@
 import SearchBrands from "../components/BrandsComponents/SearchBrands/SearchBrands";
 import BrandList from "../components/BrandsComponents/BrandList/BrandList";
-import { Suspense } from "react";
-import BrandsSkeleton from "../components/BrandsComponents/BrandsSkeleton/BrandsSkeleton";
-
 // Force dynamic rendering to prevent prerender issues
 export const dynamic = "force-dynamic";
 
@@ -12,25 +9,23 @@ const BrandsPage = async ({
   searchParams: Promise<{ query?: string }>;
 }) => {
   return (
-    <Suspense fallback={<BrandsSkeleton></BrandsSkeleton>}>
-      <div className="md:pt-[80px] pt-[58px] overflow-hidden">
-        <div className="bg-pink-500/20 px-[20px] py-[24px] flex flex-col space-y-[12px]">
-          <h1 className="md:text-center md:text-2xl font-semibold">
-            TOK offers products from top Korean brands
-          </h1>
-          <p className="md:text-center md:text-xl text-sm text-gray-500">
-            Straightly for Korea! Buy with confidence.
-          </p>
-        </div>
-        <div className="px-[3%]">
-          <div className="my-[12px]">
-            <SearchBrands />
-          </div>
-
-          <BrandList searchParams={searchParams} />
-        </div>
+    <div className="md:pt-[80px] pt-[58px] overflow-hidden">
+      <div className="bg-pink-500/20 px-[20px] py-[24px] flex flex-col space-y-[12px]">
+        <h1 className="md:text-center md:text-2xl font-semibold">
+          TOK offers products from top Korean brands
+        </h1>
+        <p className="md:text-center md:text-xl text-sm text-gray-500">
+          Straightly for Korea! Buy with confidence.
+        </p>
       </div>
-    </Suspense>
+      <div className="px-[3%]">
+        <div className="my-[12px]">
+          <SearchBrands />
+        </div>
+
+        <BrandList searchParams={searchParams} />
+      </div>
+    </div>
   );
 };
 
